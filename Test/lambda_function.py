d54278864c2eee8134fbb8cb1900ea431dd47960
Villeneuve <maxime.villeneuve@cgi.com>
2024-11-30 18:26:19 -0500

    test

--- Test/lambda_function.py
+++ Test/lambda_function.py
@@ -2,7 +2,18 @@ import json
 
 def lambda_handler(event, context):
     # TODO implement
+    display_welcome_message()
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda! ahhh')
     }
+
+# Welcome message
+def display_welcome_message():
+
+    print("""
+    ************************************************************
+    *              Welcome to the Email Extractor              *
+    *     Please wait while we process your URLs               *
+    ************************************************************
+    """)
