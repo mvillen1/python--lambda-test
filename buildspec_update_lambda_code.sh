df2528a8cf16b57a5ecac69dd44f149be1277d2c
Villeneuve <maxime.villeneuve@cgi.com>
2024-11-30 16:43:47 -0500

    test

--- buildspec_update_lambda_code.sh
+++ buildspec_update_lambda_code.sh
@@ -7,5 +7,5 @@ rsync -a  Test/ /tmp/lamda/Test/
 mv /tmp/lamda/Test/Test.py /tmp/lamda/Test/lambda_function.py
 cp .env /tmp/lamda/Test/.env
 cd /tmp/lamda/Test/ && zip -rq ../Test.zip .
-aws s3 cp /tmp/lamda/Test.zip s3://codepipeline-us-east-1-170278925856/lambda_functions/Test-Lambda/Test.zip
-aws lambda update-function-code --function-name TestLambda --s3-bucket codepipeline-us-east-1-170278925856 --s3-key lambda_functions/Test-Lambda/Test.zip
+aws s3 cp /tmp/lamda/Test.zip s3://python-test-170278925856/lambda_functions/Test-Lambda/Test.zip
+aws lambda update-function-code --function-name TestLambda --s3-bucket python-test-170278925856 --s3-key lambda_functions/Test-Lambda/Test.zip
