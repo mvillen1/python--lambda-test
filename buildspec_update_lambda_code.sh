c7309b3a28198bf26901c4a842e0885d9e43087e
Villeneuve <maxime.villeneuve@cgi.com>
2024-11-30 16:28:30 -0500

    test

---
+++ buildspec_update_lambda_code.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Lambda files got changed. Going to update lambda function code"
+aws ssm get-parameters-by-path --path /Test-LAMBDA --region us-west-2 | jq -r '.Parameters | map(.Name+"="+.Value)| join("\n") | sub("/Test-LAMBDA/"; ""; "g")  ' >.envmkdir -p /tmp/lamda/Test
+echo Copying Files from Project to Temp Folder
+rsync -a  Test/ /tmp/lamda/Test/
+mv /tmp/lamda/Test/Test.py /tmp/lamda/Test/lambda_function.py
+cp .env /tmp/lamda/Test/.env
+cd /tmp/lamda/Test/ && zip -rq ../Test.zip .
+aws s3 cp /tmp/lamda/Test.zip s3://codepipeline-us-east-1-170278925856/lambda_functions/Test-Lambda/Test.zip
+aws lambda update-function-code --function-name TestLambda --s3-bucket codepipeline-us-east-1-170278925856 --s3-key lambda_functions/Test-Lambda/Test.zip
